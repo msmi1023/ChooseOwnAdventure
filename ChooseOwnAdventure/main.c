067b2d4672f98dda3e626c08318141b9ce38df70
msmi1023 <msmi1023@ford.com>
2016-05-20 10:06:20 -0400

    Initial Commit

---
+++ ChooseOwnAdventure/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ChooseOwnAdventure
+//
+//  Created by tstone10 on 5/20/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
